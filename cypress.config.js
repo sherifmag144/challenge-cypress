25f3ecab3528cbddcd650b218e50a480beb93494
sherifmag144 <sherifmag144@gmail.com>
2025-08-22 06:21:29 +0300

    test: fromat config

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,24 @@
-// This allow you to use cypress 
-const { defineConfig } = require('cypress');
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// This allow you to use cypress
+const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 // Module thatcontain function that will make user able to export
 module.exports = defineConfig({
-  retries: {
-    runMode: 2,
-    openMode: 2,
-  },
-  env: {
-    allure: true,
-    allureReuseAfterSpec: true
+    retries: {
+        runMode: 2,
+        openMode: 2,
     },
-  video: false,
-  screenshotsFolder: 'cypress/screenshots',
-  e2e: {
-    setupNodeEvents(on, config) {
-      allureWriter(on, config);
-      return config;
+    env: {
+        allure: true,
+        allureReuseAfterSpec: true,
     },
-  },
+    video: false,
+    screenshotsFolder: "cypress/screenshots",
+    e2e: {
+        setupNodeEvents(on, config) {
+            allureWriter(on, config);
+            return config;
+        },
+    },
 });
-// Callback function 
+// Callback function
